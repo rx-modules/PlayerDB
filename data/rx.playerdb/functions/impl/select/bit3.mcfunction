07a52266fe155eaeb3c9cb89d9c7a08dd301e84c
Ritik Shah <ritik10.shah@gmail.com>
2020-11-20 13:48:08 -0800

    rx: minor tweak to bits

--- data/rx.playerdb/functions/impl/select/bit3.mcfunction
+++ data/rx.playerdb/functions/impl/select/bit3.mcfunction
@@ -2,6 +2,7 @@
 data modify storage rx:global playerdb.players[].bits.select set value 0b
 scoreboard players operation $bit rx.temp = $uid rx.temp
 scoreboard players operation $bit rx.temp %= $64 rx.int
+scoreboard players set $size rx.temp 0
 function rx.playerdb:impl/select/bit3/0_63
 scoreboard players operation $uid rx.temp /= $64 rx.int
 data modify storage rx:global playerdb.players[{bits:{select:0b}}].selected set value 0b
--- data/rx.playerdb/functions/impl/select/generate.py
+++ data/rx.playerdb/functions/impl/select/generate.py
@@ -16,7 +16,7 @@ ITERATIONS = math.log(MAX_INT, BASE) + 1
 TREE = 'execute if score $bit rx.temp matches {low}..{high} run function rx.playerdb:impl/select/bit{num}/{low}_{high}'  # noqa: E501
 
 LEAF = (
-    'execute if score $bit rx.temp matches @ if data storage rx:global playerdb.players[{bits:{b%:@b}}] store result score $size rx.temp run data modify storage rx:global playerdb.players[{selected:1b, bits:{b%:@b}}].bits.select set value 1b'  # noqa: E501
+    'execute if score $bit rx.temp matches @ if data storage rx:global playerdb.players[{selected:1b, bits:{b%:@b}}] store result score $size rx.temp run data modify storage rx:global playerdb.players[{selected:1b, bits:{b%:@b}}].bits.select set value 1b'  # noqa: E501
 )
 
 
@@ -36,10 +36,10 @@ def gen_bit(bit_num):
         'data modify storage rx:global playerdb.players[].bits.select set value 0b\n'
         'scoreboard players operation $bit rx.temp = $uid rx.temp\n'
         f'scoreboard players operation $bit rx.temp %= ${BASE} rx.int\n'
+        'scoreboard players set $size rx.temp 0\n'
         f'function rx.playerdb:impl/select/bit{bit_num}/0_{BASE-1}\n'
         f'scoreboard players operation $uid rx.temp /= ${BASE} rx.int\n'
         'data modify storage rx:global playerdb.players[{bits:{select:0b}}].selected set value 0b\n'
-        # 'execute store result score $size rx.temp if data storage rx:global playerdb.players[{selected:1b}]\n'  # noqa: E501
         f'execute if score $size rx.temp matches 2.. run function rx.playerdb:impl/select/bit{bit_num+1}\n'  # noqa: E501
     )
     fname = Path(f'bit{bit_num}.mcfunction')
